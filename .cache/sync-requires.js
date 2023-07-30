96beec513ff2b9fed3d10c360625607252d9f0d8
JuniorRodrigu <juniorrodrigues1717@gmail.com>
2023-07-30 06:23:36 -0300

    fim

---
+++ .cache/sync-requires.js
@@ -0,0 +1,11 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\Suporte Royal\\Desktop\\gatsby-starter-portfolio-cara-master\\.cache\\dev-404-page.js")),
+  "component---node-modules-lekoarts-gatsby-theme-cara-src-templates-cara-tsx": preferDefault(require("C:\\Users\\Suporte Royal\\Desktop\\gatsby-starter-portfolio-cara-master\\node_modules\\@lekoarts\\gatsby-theme-cara\\src\\templates\\cara.tsx")),
+  "component---src-pages-404-tsx": preferDefault(require("C:\\Users\\Suporte Royal\\Desktop\\gatsby-starter-portfolio-cara-master\\src\\pages\\404.tsx"))
+}
+
